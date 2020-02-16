1f4907e75b90a5439e5eba07a3f53ea970d9db9c
mtrusca <58877992+mtrusca@users.noreply.github.com>
2020-02-16 22:43:13 +0100

    Delete WildcardNodeFactory.scala

--- src/main/scala/learning/creators/WildcardNodeFactory.scala
+++
@@ -1,8 +0,0 @@
-package learning.creators
-
-import learning.core.{Terminals, TerminalNodeFactory}
-
-class WildcardNodeFactory extends TerminalNodeFactory{
-  override val terminalType: Terminals.Value = Terminals.WILDCARD
-  override val categories: Seq[String] = Seq(".*")
-}
