27394d35652cb7d18e274c53b74bed07334e3004
mtrusca <58877992+mtrusca@users.noreply.github.com>
2020-02-16 22:42:15 +0100

    Delete NegationNodeFactory.scala

--- src/main/scala/learning/creators/NegationNodeFactory.scala
+++
@@ -1,8 +0,0 @@
-package learning.creators
-
-import learning.core.{Terminals, TerminalNodeFactory}
-
-class NegationNodeFactory extends TerminalNodeFactory {
-  override val terminalType: Terminals.Value = Terminals.NEGATION
-  override val categories: Seq[String] = Seq("(not|n't)", "(however|but)")
-}
